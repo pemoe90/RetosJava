b5f4bbd19d1c569d0dff3e791ecfe3a5162da793
Eduardo Perez Moyano <pemoe90@gmail.com>
2019-03-26 21:38:01 +0100

    HolaMundo
    Reto Hola Mundo https://www.aceptaelreto.com/problem/statement.php?id=116

---
+++ HolaMundo.java
@@ -0,0 +1,14 @@
+//https://www.aceptaelreto.com/problem/statement.php?id=116
+
+import java.util.Scanner;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int repetir = scan.nextInt();
+
+        for (int i = 0; i < repetir; i++){
+            System.out.println("Hola mundo.");
+        }
+    }
+}
